66d2be384129f8a601c7d71df0caccc8a49547b7
ashok238 <uggirala.ashok@gmail.com>
2023-08-27 02:16:43 +0530

    added web authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/responsive_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb){
+ await  Firebase.initializeApp(options: FirebaseOptions(apiKey: 'AIzaSyAHLq2ovyTCsiA1UzqAcDKyeokrJjB_iPo', appId: '1:458721334666:web:fa86d16d00187206aea473', messagingSenderId: '458721334666', projectId: 'instagram-clone-7912a',storageBucket: 'instagram-clone-7912a.appspot.com'));
+ 
+  }
   runApp(const MyApp());
 }
 

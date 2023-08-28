8a9580dd93f743c0bd0ec5dafa61d6d0dc17ad6e
Ashok <ashok.uggirala@nooor.in>
2023-08-28 11:40:01 +0530

    added login ui

--- lib/responsive/web_screen.dart
+++ lib/responsive/web_screen.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:instagram_clone/screens/login_screen.dart';
+import 'package:instagram_clone/screens/signup_screen.dart';
 
 class Webscreen extends StatelessWidget {
   const Webscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('This is web'),
-      ),
-    );
+    return Scaffold(body: SignUpScreen());
   }
 }

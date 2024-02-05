1a74fa4aaf53de9dd7d1422c28c6ac94f8388410
Ashok <ashok.uggirala@nooor.in>
2024-02-05 15:43:45 +0530

    commit on feb 5

--- lib/responsive/web_screen.dart
+++ lib/responsive/web_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:instagram_clone/screens/login_screen.dart';
 import 'package:instagram_clone/screens/signup_screen.dart';
 
 class Webscreen extends StatelessWidget {
